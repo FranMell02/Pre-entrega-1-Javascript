8cab6e6c33b02919156099c1c05365c9889cf909
FrancoMellusoPaciello <137206543+FranMell02@users.noreply.github.com>
2023-08-29 23:33:24 -0300

    V.0.0.3

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 
-// Variables
-let velocidad = parseFloat(prompt("Ingresa la velocidad (en metros por segundo):"));
-let posicionInicial = parseFloat(prompt("Ingresa la posición inicial (en metros):"));
-let tiempo = parseFloat(prompt("Ingresa el tiempo (en segundos):"));
-
-// Calculo
-let posicionFinal = posicionInicial + (velocidad * tiempo);
-
-// Resultado
-alert("La posición final después de " + tiempo + " segundos es: " + posicionFinal + " metros.");
+// Función para posición final
+function calcularPosicionFinal(velocidad, posicionInicial, tiempo) {
+    return posicionInicial + (velocidad * tiempo);
+  }
+  
+  // Variables
+  let velocidad = parseFloat(prompt("Ingresa la velocidad (en metros por segundo):"));
+  let posicionInicial = parseFloat(prompt("Ingresa la posición inicial (en metros):"));
+  let tiempo = parseFloat(prompt("Ingresa el tiempo (en segundos):"));
+  
+  // Calculo
+  let posicionFinal = calcularPosicionFinal(velocidad, posicionInicial, tiempo);
+  
+  // Resultado
+  alert("La posición final después de " + tiempo + " segundos es: " + posicionFinal + " metros.");
+  
